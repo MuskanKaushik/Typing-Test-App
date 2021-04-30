b047921edcc676869d3c3aa171736c94c3bc4aed
MuskanKaushik <muskankaushik0872@gmail.com>
2021-04-30 23:45:41 +0530

    updated DOMElements

--- UIModule.js
+++ UIModule.js
@@ -4,24 +4,28 @@ var UIModule = (function(){
     var DOMElements = {
 
         //indicators - test control
-        timeLeft:'',  //HTML elememt displaying time Left
+        timeLeft:document.getElementById('timeLeft'),  //HTML elememt displaying time Left
 
         //test results
-        wpm:'', wpmChange:'',
-        cpm:'', cpmChange:'', 
-        accuracy:'', accuracyChange:'',
+        wpm: document.getElementById('wpm'), 
+        wpmChange: document.getElementById('wpmChange'),
+        cpm: document.getElementById('cpm'), 
+        cpmChange: document.getElementById('cpmChange'), 
+        accuracy:document.getElementById('accuracy'), 
+        accuracyChange:document.getElementById('accuracyChange'),
 
         //user input
-        textInput:'', 
-        nameInput:'',
+        textInput:document.querySelector('#input'), 
+        nameInput: document.querySelector('.form-group'),
 
         //test words
         content:document.getElementById('content'),
         activeWord:'',
 
         //model
-        modal:''
+        modal:$('#myModal') 
     };
+
     var splitArray = function(string){
         return string.split('');
     };
@@ -53,7 +57,9 @@ var UIModule = (function(){
         getDOMElements: function(){},
 
         //Indicators - Test Control
-        updateTimeLeft: function(){},
+        updateTimeLeft: function(x){
+           DOMElements.timeLeft.innerHTML = x;
+        },
 
         //results
         updateResults: function(){},
@@ -82,28 +88,28 @@ var UIModule = (function(){
         
             //['word1,','word2']
             var content = array.map(splitArray);
-            console.log(content);
+            //console.log(content);
 
             //[['w','o','r','d','1',','],['w','o','r','d','2']]
             content = content.map(addSpace);
-            console.log(content);
+            //console.log(content);
 
             //[['w','o','r','d','1',',',' '],['w','o','r','d','2',' ']]
 
             content = content.map(addSpanTags);
-            console.log(content);
+            //console.log(content);
             //[['<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>1</span>','<span>,</span>','<span> </span>'],['<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>2</span>','<span> </span>']]
 
             content = content.map(addWordSpanTags);
-            console.log(content);
+            //console.log(content);
             //[[<span>'<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>1</span>','<span>,</span>','<span> </span>'</span>],[<span>'<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>2</span>','<span> </span>'</span>]]
 
             content = content.map(joinEachWord);
-            console.log(content);
+            //console.log(content);
             //<span><span>w</span><span>o</span><span>r</span><span>d</span><span>1</span><span>,</span><span> </span>
             //<span>w</span><span>o</span><span>r</span><span>d</span><span>2</span><span> </span>
             content = content.join('');
-            console.log(content);
+            //console.log(content);
 
             //Replace the line return special code with a html entity (line return)
             
