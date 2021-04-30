ee391a8582bd48e0919b440e4355d6c558c481b0
MuskanKaushik <muskankaushik0872@gmail.com>
2021-04-30 17:55:22 +0530

    fill content function

--- UIModule.js
+++ UIModule.js
@@ -1,23 +1,51 @@
 var UIModule = (function(){
 
     //classes used to select HTML elements
-    //var DOMElements = {
+    var DOMElements = {
 
         //indicators - test control
-       /* timeLeft,  //HTML elememt displaying time Left
+        timeLeft:'',  //HTML elememt displaying time Left
 
         //test results
-        wpm, wpmChange, cpm, cpmChange, accuracy, accuracyChange,
+        wpm:'', wpmChange:'',
+        cpm:'', cpmChange:'', 
+        accuracy:'', accuracyChange:'',
 
         //user input
-        textInput, nameInput,
+        textInput:'', 
+        nameInput:'',
 
         //test words
-        content, activeWord,
+        content:document.getElementById('content'),
+        activeWord:'',
 
         //model
-        modal
-    };*/
+        modal:''
+    };
+    var splitArray = function(string){
+        return string.split('');
+    };
+
+    var addSpace = function(array){
+        array.push(' ')
+        return array;
+    };
+
+    var addSpanTags = function(array){
+        return array.map(function(currentCharacter){
+            return '<span>' + currentCharacter + '</span>';
+        });
+    };
+
+    var addWordSpanTags = function(array){
+        array.push('</span');
+        array.unshift('<span>');
+        return array;
+    };
+
+    var joinEachWord = function(array){
+        return array.join('');
+    };
 
     return{
 
@@ -50,7 +78,46 @@ var UIModule = (function(){
         getTypedWord: function(){},
 
         //test Words
-        fillContent: function(){},
+        fillContent: function(array, lineReturn){
+        
+            //['word1,','word2']
+            var content = array.map(splitArray);
+            console.log(content);
+
+            //[['w','o','r','d','1',','],['w','o','r','d','2']]
+            content = content.map(addSpace);
+            console.log(content);
+
+            //[['w','o','r','d','1',',',' '],['w','o','r','d','2',' ']]
+
+            content = content.map(addSpanTags);
+            console.log(content);
+            //[['<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>1</span>','<span>,</span>','<span> </span>'],['<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>2</span>','<span> </span>']]
+
+            content = content.map(addWordSpanTags);
+            console.log(content);
+            //[[<span>'<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>1</span>','<span>,</span>','<span> </span>'</span>],[<span>'<span>w</span>','<span>o</span>','<span>r</span>','<span>d</span>','<span>2</span>','<span> </span>'</span>]]
+
+            content = content.map(joinEachWord);
+            console.log(content);
+            //<span><span>w</span><span>o</span><span>r</span><span>d</span><span>1</span><span>,</span><span> </span>
+            //<span>w</span><span>o</span><span>r</span><span>d</span><span>2</span><span> </span>
+            content = content.join('');
+            console.log(content);
+
+            //Replace the line return special code with a html entity (line return)
+            
+            //<span>|</span>
+            //<span>&crarr;</span>
+            //content = content.replace('<span>|</span>','<span>&crarr;</span'); -->>> //It will replace only the first occurance of the vertical line into line return
+            
+            //The solution to replace all the vertical lines is by using split and join
+            //By split javascript will search for this separator and will return the array without this separator and join will actually take the place of the separator.
+            content = content.split('<span>'+ lineReturn + '</span>').join('<span>&crarr;</span>');
+
+            //fill content
+            DOMElements.content.innerHTML = content;
+        },
 
         formatWord: function(wordObject, wordHTML){},
 
